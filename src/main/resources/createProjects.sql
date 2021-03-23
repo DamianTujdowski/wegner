20fdd906361660495b390e0826fd2982ed538d6f
DamianTujdowski <d.tujdowski@gmail.com>
2021-03-23 23:42:21 +0100

    Added tests for filtering Projects with Specifications

---
+++ src/main/resources/createProjects.sql
@@ -0,0 +1,12 @@
+insert into project
+(id, customer, print_house, roller_size, stage, symbol, overall_preparation_duration, preparation_beginning, preparation_ending)
+values (14, 'Alicja', 'Florek', 280, 1,'21030105', 230, '2021-03-01',  '2021-03-23');
+insert into project
+(id, customer, print_house, roller_size, stage, symbol, overall_preparation_duration, preparation_beginning, preparation_ending)
+values (15, 'Alicja', 'Palst', 280, 0,'21011502', 230, '2021-01-15',  '2021-02-19');
+insert into project
+(id, customer, print_house, roller_size, stage, symbol, overall_preparation_duration, preparation_beginning, preparation_ending)
+values (16, 'York', 'Palst', 280, 1,'20122302', 230, '2020-12-23',  '2021-01-10');
+insert into project
+(id, customer, print_house, roller_size, stage, symbol, overall_preparation_duration, preparation_beginning, preparation_ending)
+values (17, 'Wist', 'Palst', 280, 1,'21030301', 230, '2021-03-03',  '2021-03-08');
--- src/test/java/pl/wegner/documents/service/ProjectServiceFilteringTest.java
+++ src/test/java/pl/wegner/documents/service/ProjectServiceFilteringTest.java
@@ -3,12 +3,10 @@ package pl.wegner.documents.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +19,8 @@ import pl.wegner.documents.utils.DateMapper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -32,6 +32,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
         locations = "classpath:documentsApplication-projectsFilteringTest.properties"
 )
 @Transactional
+@Sql(value = "classpath:createProjects.sql")
 class ProjectServiceFilteringTest {
 
     @Autowired
@@ -46,26 +47,145 @@ class ProjectServiceFilteringTest {
     @Autowired
     private ProjectSpecificationsBuilder builder;
 
-    private List<FilterCriteria> printhouseEqualsPalst = new ArrayList<>();
+    private FilterCriteria printHousePalst, printHouseFlorek, stageInAcceptation, stageProof, customerAlicja,
+            leStartSate2021_01_15, geStartDate2021_03_01;
 
     @BeforeEach
     public void setUp() {
-        FilterCriteria printhousePalst = new FilterCriteria();
-        printhousePalst.setKey("printHouse");
-        printhousePalst.setOperator("equals");
-        printhousePalst.setValue("Palst");
-        printhouseEqualsPalst.add(printhousePalst);
+        printHousePalst = new FilterCriteria();
+        printHousePalst.setKey("printHouse");
+        printHousePalst.setOperator("equals");
+        printHousePalst.setValue("Palst");
+
+        printHouseFlorek = new FilterCriteria();
+        printHouseFlorek.setKey("printHouse");
+        printHouseFlorek.setOperator("equals");
+        printHouseFlorek.setValue("Florek");
+
+        stageInAcceptation = new FilterCriteria();
+        stageInAcceptation.setKey("stage");
+        stageInAcceptation.setOperator("equals");
+        stageInAcceptation.setValue("IN_ACCEPTATION");
+
+        stageProof = new FilterCriteria();
+        stageProof.setKey("stage");
+        stageProof.setOperator("equals");
+        stageProof.setValue("PROOF");
+
+        customerAlicja = new FilterCriteria();
+        customerAlicja.setKey("customer");
+        customerAlicja.setOperator("equals");
+        customerAlicja.setValue("Alicja");
+
+        leStartSate2021_01_15 = new FilterCriteria();
+        leStartSate2021_01_15.setKey("preparationBeginning");
+        leStartSate2021_01_15.setOperator("le");
+        leStartSate2021_01_15.setValue("2021-01-15");
+
+        geStartDate2021_03_01 = new FilterCriteria();
+        geStartDate2021_03_01.setKey("preparationBeginning");
+        geStartDate2021_03_01.setOperator("ge");
+        geStartDate2021_03_01.setValue("2021-03-01");
     }
 
     @Test
-    @Sql(value = "classpath:createProjects.sql")
-    public void shouldReturn3Entities_whenSearchingForProjectPrintedByPalst() {
+    public void shouldReturnAllProjects_whenFilterCriteriaListIsEmpty() {
         //given
+        List<FilterCriteria> criteria = new ArrayList<>();
         Page<Project> result;
         //when
-        result = service.findAll(0, 20, Sort.Direction.ASC, printhouseEqualsPalst);
+        result = service.findAll(0, 20, Sort.Direction.ASC, criteria);
+        //then
+        assertEquals(4, result.getTotalElements());
+    }
+
+    @Test
+    public void shouldReturnThreeProjects_whenSearchingForProjectPrintedByPalst() {
+        //given
+        List<FilterCriteria> criteria = Stream.of(printHousePalst).collect(Collectors.toList());
+        Page<Project> result;
+        //when
+        result = service.findAll(0, 20, Sort.Direction.ASC, criteria);
         //then
         assertEquals(3, result.getTotalElements());
     }
+
+    @Test
+    public void shouldReturnOneProject_whenSearchingForProjectsPrintedByFlorek() {
+        //given
+        List<FilterCriteria> criteria = Stream.of(printHouseFlorek).collect(Collectors.toList());
+        Page<Project> result;
+        //when
+        result = service.findAll(0, 20, Sort.Direction.ASC, criteria);
+        //then
+        assertEquals(1, result.getTotalElements());
+    }
+
+    @Test
+    public void shouldReturnOneProject_whenSearchingForProjectsInStageIn_Acceptation() {
+        //given
+        List<FilterCriteria> criteria = Stream.of(stageInAcceptation).collect(Collectors.toList());
+        Page<Project> result;
+        //when
+        result = service.findAll(0, 20, Sort.Direction.ASC, criteria);
+        //then
+        assertEquals(1, result.getTotalElements());
+    }
+
+    @Test
+    public void shouldReturnThreeProjects_whenSearchingForProjectsInStageProof() {
+        //given
+        List<FilterCriteria> criteria = Stream.of(stageProof).collect(Collectors.toList());
+        Page<Project> result;
+        //when
+        result = service.findAll(0, 20, Sort.Direction.ASC, criteria);
+        //then
+        assertEquals(3, result.getTotalElements());
+    }
+
+    @Test
+    public void shouldReturnTwoProjects_whenSearchingForProjectsOrderedByCustomerAlicja() {
+        //given
+        List<FilterCriteria> criteria = Stream.of(customerAlicja).collect(Collectors.toList());
+        Page<Project> result;
+        //when
+        result = service.findAll(0, 20, Sort.Direction.ASC, criteria);
+        //then
+        assertEquals(2, result.getTotalElements());
+    }
+
+    @Test
+    public void shouldReturnOneProject_whenSearchingForProjectsOrderedByCustomerAlicjaAndPrintedByPalst() {
+        //given
+        List<FilterCriteria> criteria = Stream.of(customerAlicja, printHousePalst).collect(Collectors.toList());
+        Page<Project> result;
+        //when
+        result = service.findAll(0, 20, Sort.Direction.ASC, criteria);
+        //then
+        assertEquals(1, result.getTotalElements());
+    }
+
+    @Test
+    public void shouldReturnTwoProjects_whenSearchingForProjectsStartedAt2021_01_15OrEarlier() {
+        //given
+        List<FilterCriteria> criteria = Stream.of(leStartSate2021_01_15).collect(Collectors.toList());
+        Page<Project> result;
+        //when
+        result = service.findAll(0, 20, Sort.Direction.ASC, criteria);
+        //then
+        assertEquals(2, result.getTotalElements());
+    }
+
+    @Test
+    public void shouldReturnTwoProjects_whenSearchingForProjectsStartedAt2021_03_01OrLater() {
+        //given
+        List<FilterCriteria> criteria = Stream.of(geStartDate2021_03_01).collect(Collectors.toList());
+        Page<Project> result;
+        //when
+        result = service.findAll(0, 20, Sort.Direction.ASC, criteria);
+        //then
+        assertEquals(2, result.getTotalElements());
+    }
+
 
 }
