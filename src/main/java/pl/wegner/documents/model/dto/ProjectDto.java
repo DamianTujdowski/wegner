d2b1202e9992edde2a2319f3233042e63e2e6c6c
DamianTujdowski <d.tujdowski@gmail.com>
2021-05-08 22:41:13 +0200

    Moved countOverallPreparationDuration method from Project Service to ProjectDto

--- src/main/java/pl/wegner/documents/model/dto/ProjectDto.java
+++ src/main/java/pl/wegner/documents/model/dto/ProjectDto.java
@@ -101,4 +101,12 @@ public class ProjectDto implements Mappable<Project>{
                 .build();
 
     }
+
+    public int countOverallPreparationDuration() {
+        return this.alterations
+                .stream()
+                .mapToInt(Alteration::getDuration)
+                .sum();
+    }
+
 }
